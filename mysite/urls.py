6215d7d1c926db53adf18a815f3d8b74bf333ee7
UserName <spam.mieszkanie@gmail.com>
2018-09-18 22:19:28 +0200

    Backup

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.urls import path, include
 from django.contrib import admin
 
+from django.contrib.auth import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path(r'^accounts/login/$', views.login, name='login'),
+    path(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+
 ]
